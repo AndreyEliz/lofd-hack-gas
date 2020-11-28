bc4196c0847f388f87d6b2ca4cb3e638baa73070
Arca\OArca <braunbrustigel@gmail.com>
2020-11-28 21:52:12 +0300

    add date to job items

--- client/src/pages/JobsPage.tsx/JobListItem/JobListItem.tsx
+++ client/src/pages/JobsPage.tsx/JobListItem/JobListItem.tsx
@@ -11,27 +11,48 @@ import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { useLocation } from 'hooks/router.hooks';
+import Tooltip from '@material-ui/core/Tooltip';
 
 interface IJobListItemProps {
     data: IJob
+}
+
+const getTooltipTitle = (data:IJob) => {
+    if (data.status === 'new') return 'Новая заявка';
+    if (data.status === 'open') return 'Заявка открыта, но не опубликована';
+    if (data.status === 'published') return 'Заявка опубликована';
+    if (data.status === 'closed') return 'Есть прошедшие все этапы соискатели';
+    else return ''
 }
 
 const JobListItem: React.FC<IJobListItemProps> = ({data}) => {
     const classes = useStyles(data)
     const {navigate} = useLocation()
 
+    const tooltipTitle = getTooltipTitle(data)
+    const openDate = data.openDate.toLocaleDateString()
+    const publishDate = data.publishedDate?.toLocaleDateString()
+    const title = (
+    <span >
+        {data.title}
+        <span className={classes.dateHeader}>{` - открыто ${openDate}`}</span>
+        <span className={classes.dateHeader}>{data.publishedDate && `,  опубликовано ${publishDate}`}</span>
+    </span>)
+
     return (
         <div className={classes.jobWrapper}>
             <CardCustom 
                 collapsable={false}
-                title={data.title} 
+                title={title} 
                 avatar={
-                <Avatar aria-label="recipe" className={classes.avatar}>
-                    {data.status === 'new' && <FiberNewIcon/>}
-                    {data.status === 'open' && <NewReleasesIcon/>}
-                    {data.status === 'published' && <PublicIcon/>}
-                    {data.status === 'closed' && <ThumbUpIcon/>}
-                </Avatar>
+                <Tooltip title={tooltipTitle}>
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                        {data.status === 'new' && <FiberNewIcon/>}
+                        {data.status === 'open' && <NewReleasesIcon/>}
+                        {data.status === 'published' && <PublicIcon/>}
+                        {data.status === 'closed' && <ThumbUpIcon/>}
+                    </Avatar>
+                </Tooltip>
             }>
                 <CardContent>
                     <div className={classes.cardContent}>
