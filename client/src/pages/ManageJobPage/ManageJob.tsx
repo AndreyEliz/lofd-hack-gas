fa1ca950eb53d99cf5a032bed0da038d3523cf81
Arca\OArca <braunbrustigel@gmail.com>
2020-11-28 05:58:02 +0300

    add benefits to manageJob page

--- client/src/pages/ManageJobPage/ManageJob.tsx
+++ client/src/pages/ManageJobPage/ManageJob.tsx
@@ -1,14 +1,14 @@
-import Vacancy from 'components/Vacancy/Vacancy';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { IJob } from 'store/models/jobModel';
 import { selectJobList } from 'reducers/jobList.reducer';
-import { useParams } from '../../hooks/router.hooks';
+import { useParams } from 'hooks/router.hooks';
 import CardCustom from 'components/CardCustom/CardCustom';
 import { CardContent } from '@material-ui/core';
-import VacancyCoreData from '../../components/Vacancy/VacancyCoreData';
+import VacancyCoreData from 'components/Vacancy/VacancyCoreData';
 import Typography from '@material-ui/core/Typography';
 import ChipsField from 'components/ChipsField/ChipsField';
+import CandidateList from './CandidatesList/CandidatesList';
 
 
 const ManageJobPage: React.FC = () => {
@@ -19,14 +19,22 @@ const ManageJobPage: React.FC = () => {
         <div>
             <CardCustom title={vacancy.title} defaultOpen={false}>
                 <CardContent>
+                    {vacancy.type &&
                     <div>
-                        {vacancy.type &&
-                        <ChipsField options={vacancy.type.map((type: string) => `#${type}`)} color='default'/>}
+                        <ChipsField options={vacancy.type.map((type: string) => `#${type}`)} color='default'/>
                         <div dangerouslySetInnerHTML={{ __html: vacancy.summary }} />
-                    </div>
+                    </div>}
                     <VacancyCoreData data={vacancy}/>
+                    {vacancy.benefits && 
+                    <div>
+                        <Typography component="div"  variant="body2" color="textSecondary">
+                            Бонусы:
+                        </Typography>
+                        <div dangerouslySetInnerHTML={{ __html: vacancy.benefits }} />
+                    </div>}
                 </CardContent>
             </CardCustom>
+            <CandidateList/>
         </div>
     );
 }
