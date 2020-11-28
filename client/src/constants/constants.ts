370cde41477ec7749b02981b979790632f55de74
Arca\OArca <braunbrustigel@gmail.com>
2020-11-28 19:09:46 +0300

    add new skills

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,6 +1,8 @@
 export const SKILL_LEVELS = ['Junior', 'Middle', 'Senior']
 export const WORK_TYPES = ['Remote', 'Fulltime', 'Parttime']
 export const SKILLS = [
+    'Web-Design',
+    'Design',
     'Abode',
     'Adobe Acrobat',
     'Adobe Illustrator',
