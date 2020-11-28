5342e0522d422d6881ac46a4c82ad531a914af35
Arca\OArca <braunbrustigel@gmail.com>
2020-11-28 05:38:41 +0300

    fix id duplication

--- client/src/store/mockData/jobs.ts
+++ client/src/store/mockData/jobs.ts
@@ -23,7 +23,7 @@ JOBS.push({...testJob, status: 'open', id: 2})
 JOBS.push({...testJob, status: 'published', id: 3})
 JOBS.push({...testJob, status: 'closed', id: 4})
 
-for (let i = JOBS.length; i <= 10; i++) {
+for (let i = JOBS.length+1; i <= 10; i++) {
     JOBS.push({...testJob, id: i})
 }
 
