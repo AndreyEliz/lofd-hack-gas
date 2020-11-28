b6608277570bd7a4a65096ca0160c5ea76d8ec47
Arca\OArca <braunbrustigel@gmail.com>
2020-11-28 05:38:59 +0300

    minor refactoring

--- client/src/components/Vacancy/Vacancy.tsx
+++ client/src/components/Vacancy/Vacancy.tsx
@@ -1,41 +1,16 @@
 import React from 'react';
 import { IJob } from 'store/models/jobModel';
 import { useStyles } from './styles';
-import { ClassNameMap } from '@material-ui/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import {ILanguage} from "store/models/helpers";
-import Chip from '@material-ui/core/Chip';
 import CardCustom from "components/CardCustom/CardCustom";
-
-interface IChipsField {
-    classes: ClassNameMap<any>;
-    options: string[];
-    color?: 'primary' | 'secondary' | 'default';
-    id: string;
-}
-
+import VacancyCoreData from './VacancyCoreData';
+import ChipsField from 'components/ChipsField/ChipsField';
 interface IVacancyProps {
     data: IJob,
     defaultOpen?: boolean
 }
-
-const ChipsField = ({ classes, options, color = 'primary', id }: IChipsField) => {
-    return (
-        <div className={classes.wrapperChips}>
-            {options.map((option: string, idx: number) => (
-                <Chip
-                    key={`chip-${id}-${idx}`}
-                    label={option}
-                    color={color}
-                    variant="outlined"
-                    size="small"
-                />
-            ))}
-        </div>
-    );
-};
 
 const Vacancy: React.FC<IVacancyProps> = ({data, defaultOpen=true}) => {
     const classes = useStyles();
@@ -43,22 +18,15 @@ const Vacancy: React.FC<IVacancyProps> = ({data, defaultOpen=true}) => {
     const {
         title,
         summary,
-        mainSkills,
-        secondarySkills,
         skillLevel,
         type,
         salaryRate,
-        languages,
         benefits
     } = data;
 
     const salary = salaryRate ? `${salaryRate.from || '...'} ~ ${salaryRate.to || '...'} руб.` : '';
-    const languagesList = languages && (
-        <ChipsField id='languages' classes={classes} options={languages.map(({name, level}: ILanguage) => `${name}: ${level.toLowerCase()}`)}/>
-    );
-    const mainSkillsList = mainSkills && <ChipsField id='main-skillss' classes={classes} options={mainSkills}/>;
-    const secondarySkillsList = secondarySkills && <ChipsField id='secondary-skills' classes={classes} options={secondarySkills} color='secondary'/>;
-    const typeList = type && <ChipsField id='types' classes={classes} options={type.map((type: string) => `#${type}`)} color='default'/>;
+
+    const typeList = type && <ChipsField options={type.map((type: string) => `#${type}`)} color='default'/>;
 
     return (
         <CardCustom defaultOpen={defaultOpen} title={`${title} (${skillLevel}) ... ${salary}`}>
@@ -66,23 +34,18 @@ const Vacancy: React.FC<IVacancyProps> = ({data, defaultOpen=true}) => {
                 <Grid item xs={12} sm container>
                     <Grid item xs container direction="column" spacing={2}>
                         <Grid item xs>
-                            <Typography variant="body2" gutterBottom>
+                            <div>
                                 {typeList}
                                 <div dangerouslySetInnerHTML={{ __html: summary }} />
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">Основные скиллы: {mainSkillsList}</Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Будет плюсом следующие скиллы: {secondarySkillsList}
-                            </Typography>
-                            {languagesList && (
-                                <Typography variant="body2" color="textSecondary">
-                                    Иностранные языки: {languagesList}
-                                </Typography>
-                            )}
+                            </div>
+                            <VacancyCoreData data={data}/>
                             {benefits && (
-                                <Typography variant="body2" color="textSecondary">
-                                    Бонусы: <div dangerouslySetInnerHTML={{ __html: benefits }} />
+                                <>
+                                <Typography component="div"  variant="body2" color="textSecondary">
+                                    Бонусы:
                                 </Typography>
+                                <div dangerouslySetInnerHTML={{ __html: benefits }} />
+                                </>
                             )}
                             <Button
                                 variant="contained"
