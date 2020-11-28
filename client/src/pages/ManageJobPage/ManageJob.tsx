8233ebab074e3b5e01ff99001c1c09b507f0a022
Arca\OArca <braunbrustigel@gmail.com>
2020-11-28 05:39:13 +0300

    manageJob page setup

--- client/src/pages/ManageJobPage/ManageJob.tsx
+++ client/src/pages/ManageJobPage/ManageJob.tsx
@@ -4,15 +4,29 @@ import { useSelector } from 'react-redux';
 import { IJob } from 'store/models/jobModel';
 import { selectJobList } from 'reducers/jobList.reducer';
 import { useParams } from '../../hooks/router.hooks';
+import CardCustom from 'components/CardCustom/CardCustom';
+import { CardContent } from '@material-ui/core';
+import VacancyCoreData from '../../components/Vacancy/VacancyCoreData';
+import Typography from '@material-ui/core/Typography';
+import ChipsField from 'components/ChipsField/ChipsField';
 
 
 const ManageJobPage: React.FC = () => {
     const {id} = useParams()
-    const vacancy = useSelector(selectJobList).find((job:IJob) => job.id == id)
+    const vacancy: IJob = useSelector(selectJobList).find((job:IJob) => job.id == id)
 
     return (
         <div>
-            <Vacancy defaultOpen={false} data={vacancy}/>
+            <CardCustom title={vacancy.title} defaultOpen={false}>
+                <CardContent>
+                    <div>
+                        {vacancy.type &&
+                        <ChipsField options={vacancy.type.map((type: string) => `#${type}`)} color='default'/>}
+                        <div dangerouslySetInnerHTML={{ __html: vacancy.summary }} />
+                    </div>
+                    <VacancyCoreData data={vacancy}/>
+                </CardContent>
+            </CardCustom>
         </div>
     );
 }
